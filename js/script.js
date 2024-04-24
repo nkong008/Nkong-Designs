186d4e5cecf9205962fa5904e4805293dbf7adfa
nkong008 <nkong2020@gmail.com>
2024-04-23 19:09:10 -0500

    new repo for public version

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+
+$(document).ready(function(){
+    let navLinks = document.querySelectorAll(".nav-links");
+    console.log(navLinks);
+    $("#menu").click(function() {
+        $("#navigation").toggle(animateLinks());
+    });
+    function animateLinks() {
+        gsap.from($("#navigation"), { y: -100, opacity:0, duration: .25})
+    }
+});
+
